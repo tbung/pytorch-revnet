cd53b38245067412a3cc9fd33c560711dd9f5f3f
Till Bungert <tillbungert@gmail.com>
2017-08-31 11:44:37 +0200

    Fix residual for possible downsample in basic block.

--- models/resnet.py
+++ models/resnet.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -9,17 +8,15 @@ class Block(nn.Module):
 
         self.stride = stride
 
-
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                          padding=1, stride=stride)
+                               padding=1, stride=stride)
 
         self.bn1 = nn.BatchNorm2d(out_channels)
 
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
-                          padding=1)
+                               padding=1)
 
         self.bn2 = nn.BatchNorm2d(out_channels)
-
 
         if stride != 1:
             self.res = nn.Sequential(
@@ -27,14 +24,15 @@ class Block(nn.Module):
                               stride=stride, bias=False),
                     nn.BatchNorm2d(out_channels)
                     )
+        else:
+            self.res = lambda x: x
 
     def forward(self, x):
         orig_x = x
 
         out = F.relu(self.bn1(self.conv1(x)))
 
-        if self.res is not None:
-            orig_x = self.res(orig_x)
+        orig_x = self.res(orig_x)
 
         out = F.relu(self.bn2(self.conv2(x)) + orig_x)
 
