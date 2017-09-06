a03446ffa24566606c1e7936d54e3d1b0b683202
Till Bungert <tillbungert@gmail.com>
2017-09-06 21:52:38 +0200

    Add revnet code skeleton

---
+++ models/revnet.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicBlock(nn.Module):
+    pass
+
+
+class Bottleneck(nn.Module):
+    pass
+
+
+class RevNet(nn.Module):
+    pass
