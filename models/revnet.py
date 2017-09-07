26347f5f851a026b8849e2afde2252d79c5cf8ff
Till Bungert <tillbungert@gmail.com>
2017-09-07 21:41:56 +0200

    Add testing module for revnet
    Currently intended to be importet in an ipython session to experiment
    on.

--- models/revnet.py
+++ models/revnet.py
@@ -1,12 +1,38 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+from torch.autograd import Function
+
+from .resnet import Block
 
 
-class BasicBlock(nn.Module):
-    pass
+# class RevBlockFunction(Function):
+#     @staticmethod
+#     def forward(ctx, x,
 
 
-class Bottleneck(nn.Module):
+class RevBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(RevBlock, self).__init__()
+
+        self.f = Block(in_channels, out_channels, stride)
+        self.g = Block(out_channels, out_channels, stride=1)
+
+    def forward(self, x):
+        x1, x2 = torch.chunk(x, 2, dim=3)
+
+        f_x2 = self.f(x2)
+
+        y1 = f_x2 + x1
+
+        g_y1 = self.g(y1)
+
+        y2 = g_y1 + x2
+
+        return torch.cat([y1, y2], dim=3)
+
+
+class RevBottleneck(nn.Module):
     pass
 
 
---
+++ test/test_revnet.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from torch.autograd import Variable
+
+import models.revnet
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+
+test_block = models.revnet.RevBlock(3, 3)
