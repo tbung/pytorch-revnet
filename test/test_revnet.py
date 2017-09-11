7d63a1a356db515907932311c1ebc30da34c7819
Till Bungert <tillbungert@gmail.com>
2017-09-11 19:30:27 +0200

    Remove call to old test module

--- test/test_revnet.py
+++ test/test_revnet.py
@@ -1,10 +1,21 @@
-import test.test_revnet as t
 import visualize
+import torch
+import torchvision
+import torchvision.transforms as transforms
 from torch.autograd import Variable
 from models.revnet import RevNet
 
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
 test = RevNet([3, 3, 3], [16, 16, 32, 64], [1, 1, 1], 10)
-dataiter = iter(t.trainloader)
+dataiter = iter(trainloader)
 images, labels = dataiter.next()
 output = test(Variable(images))
 g = visualize.make_dot(output)
