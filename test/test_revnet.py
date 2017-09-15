50328c5f89e7450e8abc5a60412898c172857083
Till Bungert <tillbungert@gmail.com>
2017-09-15 22:37:31 +0200

    WIP test changes

--- test/test_revnet.py
+++ test/test_revnet.py
@@ -3,9 +3,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+from resnet import ResNet18
 import torchvision.transforms as transforms
-from torch.autograd import Variable  # , gradcheck
+from torch.autograd import Variable, gradcheck
 from models.revnet import RevNet, RevBlock, RevGroupFunction
+from models import revnet38
+# from models.resnet import ResNet
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -16,31 +19,51 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
-test = RevNet([3, 3, 3], [16, 16, 32, 64], [2, 1, 1], 10)
+# test = RevNet([3], [16, 16], [2], 10)
+test = ResNet18()
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 output = test(Variable(images))
-g = visualize.make_dot(output)
-g.view()
+_, predicted = torch.max(output.data, 1)
+print(predicted)
+# g = visualize.make_dot(output)
+# g.view()
+images, labels = dataiter.next()
+output = test(Variable(images))
+_, predicted = torch.max(output.data, 1)
+print(predicted)
+images, labels = dataiter.next()
+output = test(Variable(images))
+_, predicted = torch.max(output.data, 1)
+print(predicted)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(test.parameters(), lr=0.1,
                       momentum=0.9, weight_decay=0.0001)
 
 loss = criterion(output, Variable(labels))
+# print(test.groups[0][1].f.conv1.bias)
+# print(test.groups[0][1].f.conv1.bias.grad)
+w = [torch.Tensor(i.data) for i in list(test.parameters())]
 loss.backward()
+# print(test.groups[0][1].f.conv1.bias)
+# print(test.groups[0][1].f.conv1.bias.grad)
+# print(test.layers[2].conv1.bias)
 optimizer.step()
-
-
-# gradchek takes a tuple of tensor as input, check if your gradient
-# evaluated with these tensors are close enough to numerical
-# approximations and returns True if they all verify this condition.
-# input = (Variable(torch.rand(4, 16, 32, 32), requires_grad=True),
-#          nn.ModuleList([RevBlock(16, 16), RevBlock(16, 16)]))
-# # test = gradcheck(RevGroupFunction.apply, input, eps=1e-6, atol=1e-4)
-# test = RevGroupFunction.apply(*input)
-# # print(test)
-# loss = nn.L1Loss()
-# target = Variable(torch.rand(4, 16, 32, 32))
-# t = loss(test, target)
-# t.backward()
+# print(test.groups[0][1].f.conv1.bias)
+# print(test.groups[0][1].f.conv1.bias.grad)
+output = test(Variable(images))
+_, predicted = torch.max(output.data, 1)
+print(predicted)
+images, labels = dataiter.next()
+output = test(Variable(images))
+_, predicted = torch.max(output.data, 1)
+print(predicted)
+images, labels = dataiter.next()
+output = test(Variable(images))
+_, predicted = torch.max(output.data, 1)
+print(predicted)
+n, params = zip(*list(test.named_parameters()))
+for i, p in enumerate(params):
+    if not w[i].equal(p.data):
+        print(n[i])
