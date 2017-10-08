4c24dd1e4b1a41fe1b9493eb5e22f363acde31f0
Till Bungert <tillbungert@gmail.com>
2017-10-08 17:38:54 +0200

    Remove broken tests and start over

--- tests/test_revnet.py
+++ tests/test_revnet.py
@@ -2,8 +2,6 @@ import torch
 import torch.autograd
 from torch.autograd import Variable
 
-# import visualize
-
 import models.revnet as revnet
 
 import unittest
@@ -16,23 +14,7 @@ class TestRevNet(TestCase):
         pass
 
     def test_grad(self):
-        act = []
-        net = revnet.RevBlock(2, 2, act)
-        x = Variable(torch.rand(1, 2, 4, 4), requires_grad=True)
-        act.append(net(x).data)
-        inputs = [
-            x,
-            net.in_channels,
-            net.out_channels,
-            net.training,
-            net.stride,
-            net.no_activation,
-            net.activations,
-            *net._parameters.values(),
-            *net._buffers.values()
-        ]
-        test = torch.autograd.gradcheck(revnet.RevBlockFunction.apply, inputs)
-        print(test)
+        pass
 
     def test_recreation(self):
         pass
